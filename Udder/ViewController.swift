c55e12c70df3db772d7c0e5491ee3c4ad6a6e92e
rnjstjrrl <138309683+rnjstjrrl@users.noreply.github.com>
2023-07-01 23:51:43 +0900

    Update ViewController.swift

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
+    
 }
 
